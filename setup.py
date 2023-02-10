3bb27032980c0a11add4a873d03dc856b2791896
Julie Keisler <46896800+JulieKeisler@users.noreply.github.com>
2023-02-10 09:56:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
     name="evodags",
     version="1.0",
-    packages=find_packages(),
+    description='Implementation of the algorithmic framework from <An algorithmic framework for the optimization of deep neural networks architectures and hyperparameters>',
+    packages=['evodags'],
+    author='Julie Keisler',
+    author_email='julie.keisler@edf.fr',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'zellij',
+        'torch'
+        'glutons[torch,pro]'
+    ],
 )
