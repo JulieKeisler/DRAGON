552aa2dbc1a5d265838a470dbbe5a527ddd3853e
Julie Keisler <46896800+JulieKeisler@users.noreply.github.com>
2023-10-11 17:11:49 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,117 +1,38 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
+# -- Project information
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
+project = 'DRAGON'
+copyright = '2023, Julie Keisler'
+author = 'Julie Keisler'
 
-import os
-import sys
+release = '0.1'
+version = '0.1.0'
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("../"))
-sys.path.insert(
-    1, os.path.dirname(os.path.abspath("../")) + os.sep + "feature_engine"
-)
-sys.path.insert(0, os.path.abspath("../.."))
+# -- General configuration
 
-# -- Project information -----------------------------------------------------
-
-project = "dragon"
-copyright = "2023, Julie Keisler"
-author = "Julie Keisler"
-
-# The full version, including alpha/beta/rc tags
-release = "1.0.0"
-
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.mathjax",
-    "sphinx.ext.imgmath",
-    "sphinx_rtd_theme",
-    "sphinx_copybutton",
-    "sphinx_togglebutton",
-]
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [
-    "*/tests",
-    "setup.py",
-    "test_*.py",
-    "_build",
-    "Thumbs.db",
-    ".DS_Store",
-    "**.ipynb_checkpoints",
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
 ]
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "sphinx_book_theme"
-html_title = ""
-html_logo = "dragon_logo.jpeg"
-html_theme_options = {
-    "repository_url": "https://github.com/JulieKeisler/EvoDagsAutoDL",
-    "use_repository_button": True,
-    "collapse_navigation": False,
-    "logo_only": True,
-    "extra_navbar": f"<p>Version: {release}</p>",
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
+intersphinx_disabled_domains = ['std']
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+templates_path = ['_templates']
 
-# If true, the current module name will be prepended to all description
-# unit titles (such as .. function::).
-add_module_names = False
+# -- Options for HTML output
 
-autodoc_member_order = "bysource"
+html_theme = 'sphinx_rtd_theme'
+html_logo = 'dragon_logo.jpeg'
 
 
-def setup(app):
-    # -- To demonstrate ReadTheDocs switcher -------------------------------------
-    # This links a few JS and CSS files that mimic the environment that RTD uses
-    # so that we can test RTD-like behavior. We don't need to run it on RTD and we
-    # don't wanted it loaded in GitHub Actions because it messes up the lighthouse
-    # results.
-    if not os.environ.get("READTHEDOCS") and not os.environ.get(
-        "GITHUB_ACTIONS"
-    ):
-        app.add_css_file(
-            "https://assets.readthedocs.org/static/css/readthedocs-doc-embed.css"
-        )
-        app.add_css_file(
-            "https://assets.readthedocs.org/static/css/badge_only.css"
-        )
 
-        # Create the dummy data file so we can link it
-        # ref: https://github.com/readthedocs/readthedocs.org/blob/bc3e147770e5740314a8e8c33fec5d111c850498/readthedocs/core/static-src/core/js/doc-embed/footer.js  # noqa: E501
-        app.add_js_file("rtd-data.js")
-        app.add_js_file(
-            "https://assets.readthedocs.org/static/javascript/readthedocs-doc-embed.js",
-            priority=501,
-        )
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
